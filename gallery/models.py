b147c11afbf24095061c04aeb9f4ba546e2fc952
mugambi254 <mugambi254@localhost.localdomain>
2019-05-18 11:49:27 +0300

    create image,category,location models

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    title = models.CharField(max_length =60)
+    description = models.CharField(max_length=100)
+    location= models.ForeignKey(Location, on_delete=models.CASCADE)
+    category= models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
