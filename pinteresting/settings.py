ad790e2ce2ce1aa2fa45722d4989bb4980303db5
mugambi254 <mugambi254@localhost.localdomain>
2019-05-22 12:51:22 +0300

    initial commit to heroku

--- pinteresting/settings.py
+++ pinteresting/settings.py
@@ -62,6 +62,8 @@ db_from_env=dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
+DEBUG = config('DEBUG', default=False, cast=bool)
+
 # Application definition
 
 INSTALLED_APPS = [
